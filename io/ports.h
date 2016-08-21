0ae115ee094191f23cae2ee151a96619a8ae6e0d
vodozhaba <vodozhaba@vodozhaba.net>
2016-08-21 17:08:14 +0300

    fixed undefined reference

--- io/ports.h
+++ io/ports.h
@@ -9,31 +9,31 @@
 
 #include "stdint.h"
 
-inline void PortWrite8(uint16_t port, uint8_t value) {
+static inline void PortWrite8(uint16_t port, uint8_t value) {
     __asm volatile("outb %b0, %w1" : : "a" (value), "d" (port));
 }
 
-inline uint8_t PortRead8(uint16_t port) {
+static inline uint8_t PortRead8(uint16_t port) {
     uint8_t ret;
     __asm volatile("inb %w1, %b0" : "=a" (ret) : "d" (port));
     return ret;
 }
 
-inline void PortWrite16(uint16_t port, uint16_t value) {
+static inline void PortWrite16(uint16_t port, uint16_t value) {
     __asm volatile("outw %w0, %w1" : : "a" (value), "d" (port));
 }
 
-inline uint16_t PortRead16(uint16_t port) {
+static inline uint16_t PortRead16(uint16_t port) {
     uint16_t ret;
     __asm volatile("inw %w1, %w0" : "=a" (ret) : "d" (port));
     return ret;
 }
 
-inline void PortWrite32(uint16_t port, uint32_t value) {
+static inline void PortWrite32(uint16_t port, uint32_t value) {
     __asm volatile("outl %w0, %w1" : : "a" (value), "d" (port));
 }
 
-inline uint32_t PortRead32(uint16_t port) {
+static inline uint32_t PortRead32(uint16_t port) {
     uint32_t ret;
     __asm volatile("inl %w1, %w0" : "=a" (ret) : "d" (port));
     return ret;
