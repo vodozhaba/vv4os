a49b7e72f15d09006c22523d6337cbeb5a27aa8c
vodozhaba <vodozhaba@vodozhaba.net>
2016-08-21 14:28:42 +0300

    added functions for wide port I/O

--- io/ports.h
+++ io/ports.h
@@ -18,3 +18,23 @@ inline uint8_t PortRead8(uint16_t port) {
     __asm volatile("inb %w1, %b0" : "=a" (ret) : "d" (port));
     return ret;
 }
+
+inline void PortWrite16(uint16_t port, uint16_t value) {
+    __asm volatile("outw %w0, %w1" : : "a" (value), "d" (port));
+}
+
+inline uint16_t PortRead16(uint16_t port) {
+    uint16_t ret;
+    __asm volatile("inw %w1, %w0" : "=a" (ret) : "d" (port));
+    return ret;
+}
+
+inline void PortWrite32(uint16_t port, uint32_t value) {
+    __asm volatile("outl %w0, %w1" : : "a" (value), "d" (port));
+}
+
+inline uint32_t PortRead32(uint16_t port) {
+    uint32_t ret;
+    __asm volatile("inl %w1, %w0" : "=a" (ret) : "d" (port));
+    return ret;
+}
