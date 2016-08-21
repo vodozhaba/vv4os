f10f5abb31b8ca31d4e68c97217154c29891dc82
vodozhaba <vodozhaba@vodozhaba.net>
2016-08-22 00:11:10 +0300

    added FPU initializer

---
+++ proc_exts/fpu.h
@@ -0,0 +1,12 @@
+/******************************************************************************
+ * File:       /vv4os/proc_exts/fpu.h                                         *
+ * Author:     velikiyv4                                                      *
+ * Created on: Aug 21, 2016                                                   *
+ * Purpose:    Provides interface for FPU if there is one.                    *
+ ******************************************************************************/
+
+#pragma once
+
+#include <stdbool.h>
+
+bool HasFpu();
