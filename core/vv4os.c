27d487ff336cb071512383c0538694d211d068fe
vodozhaba <vodozhaba@vodozhaba.net>
2016-08-21 17:41:26 +0300

    removed test

--- core/vv4os.c
+++ core/vv4os.c
@@ -1,5 +1,5 @@
 /******************************************************************************
- * File:       /vv4os/core/vv4os.c                                                   *
+ * File:       /vv4os/core/vv4os.c                                            *
  * Author:     velikiyv4                                                      *
  * Created on: Aug 20, 2016                                                   *
  * Purpose:    The main file of the project, connects all the modules.        *
@@ -9,10 +9,4 @@
 
 void main() {
     VgaTerminalInit();
-    VgaTerminalPut('A');
-    vga_color_scheme red = {
-            .foreground = VGA_COLOR_RED,
-            .background = VGA_COLOR_BLACK
-    };
-    VgaTerminalSwitchColorScheme(red);
 }
