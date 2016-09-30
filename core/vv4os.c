4aad43e705a25e34813817c585e1d91614f757e5
vodozhaba <vodozhaba@vodozhaba.net>
2016-09-30 20:11:46 +0300

    deleted GDT code (it wouldn't work)

--- core/vv4os.c
+++ core/vv4os.c
@@ -5,15 +5,12 @@
  * Purpose:    The main file of the project, connects all the modules.        *
  ******************************************************************************/
 
-#include "arch/x86/gdt.h"
 #include "io/vga_terminal.h"
 #include "stdlib/stdio.h"
 
 static void InitAll() {
     VgaTerminalInit();
     printf("Initialized VGA terminal\n");
-    InitGdt();
-    printf("Initialized GDT\n");
 }
 
 void main() {
