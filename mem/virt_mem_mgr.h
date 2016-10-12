ca09cbb70b004d81e50927d8c55b8d365a618307
vodozhaba <vodozhaba@vodozhaba.net>
2016-10-12 17:24:04 +0300

    deleted paging (it's main idea was bad)

--- mem/virt_mem_mgr.h
+++ mem/virt_mem_mgr.h
@@ -7,34 +7,11 @@
 
 #pragma once
 
-void* X86CreateKernelAndProcWindow(void* proc_base, uint32_t frames,
-        uint32_t proc_id);
-void X86NewVirtMemTable(uint32_t proc_id);
-int X86DestroyVirtMemTable(uint32_t proc_id);
 void X86VirtMemMgrInit();
 
 #if !defined(__X86__)
 #error "Cannot determine target architecture"
 #endif
-
-static inline void* CreateKernelAndProcWindow(void* proc_base, uint32_t frames,
-        uint32_t proc_id) {
-#if defined(__X86__)
-    return X86CreateKernelAndProcWindow(proc_base, frames, proc_id);
-#endif
-}
-
-static inline void NewVirtMemTable(uint32_t proc_id) {
-#if defined(__X86__)
-    X86NewVirtMemTable(proc_id);
-#endif
-}
-
-static inline int DestroyVirtMemTable(uint32_t proc_id) {
-#if defined(__X86__)
-    return X86DestroyVirtMemTable(proc_id);
-#endif
-}
 
 static inline void VirtMemMgrInit() {
 #if defined(__X86__)
