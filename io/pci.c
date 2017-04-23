86d6df7bcdc50f1ec6b9c4f83d24dd733d820632
vodozhaba <vodozhaba@vodozhaba.net>
2017-04-23 20:39:05 +0000

    made the device list public

--- io/pci.c
+++ io/pci.c
@@ -43,42 +43,7 @@ typedef struct {
 	bool mf : 1;
 } __attribute__((packed)) PciConfigHeaderType;
 
-typedef struct PciDevice {
-	uint16_t vendor_id;
-	uint16_t device_id;
-	uint16_t command;
-	uint16_t status;
-	uint8_t revision_id;
-	uint8_t prog_if;
-	uint8_t subclass;
-	uint8_t class_code;
-	uint8_t cache_line_size;
-	uint8_t latency_timer;
-	uint8_t header_type;
-	uint8_t bist;
-	uint32_t bar0;
-	uint32_t bar1;
-	uint32_t bar2;
-	uint32_t bar3;
-	uint32_t bar4;
-	uint32_t bar5;
-	uint32_t cardbus_cis_pointer;
-	uint8_t subsystem_vendor_id;
-	uint8_t sybsystem_id;
-	uint32_t expansion_rom_base_address;
-	uint8_t capabilities_pointer;
-	uint64_t reserved : 56;
-	uint8_t interrupt_line;
-	uint8_t interrupt_pin;
-	uint8_t min_grant;
-	uint8_t max_latency;
-	uint8_t bus;
-	uint8_t device : 5;
-	uint8_t function : 3;
-	struct PciDevice* next;
-} __attribute__((packed)) PciDevice;
-
-static PciDevice* devices_list = NULL;
+PciDevice* pci_devices_list = NULL;
 
 static uint32_t PciConfigRead32(uint8_t bus, uint8_t device, uint8_t function, uint8_t offset) {
 	PciConfigAddress addr;
@@ -120,8 +85,8 @@ static void PciAddDevice(uint8_t bus, uint8_t device, uint8_t function) {
 	pci_device->bus = bus;
 	pci_device->device = device;
 	pci_device->function = function;
-	pci_device = devices_list;
-	devices_list = pci_device;
+	pci_device->next = pci_devices_list;
+	pci_devices_list = pci_device;
 }
 
 static void PciProbeFunction(uint8_t bus, uint8_t device, uint8_t function) {
--- io/pci.h
+++ io/pci.h
@@ -7,4 +7,43 @@
 
 #pragma once
 
+#include <stdint.h>
+
+typedef struct PciDevice {
+	uint16_t vendor_id;
+	uint16_t device_id;
+	uint16_t command;
+	uint16_t status;
+	uint8_t revision_id;
+	uint8_t prog_if;
+	uint8_t subclass;
+	uint8_t class_code;
+	uint8_t cache_line_size;
+	uint8_t latency_timer;
+	uint8_t header_type;
+	uint8_t bist;
+	uint32_t bar0;
+	uint32_t bar1;
+	uint32_t bar2;
+	uint32_t bar3;
+	uint32_t bar4;
+	uint32_t bar5;
+	uint32_t cardbus_cis_pointer;
+	uint8_t subsystem_vendor_id;
+	uint8_t sybsystem_id;
+	uint32_t expansion_rom_base_address;
+	uint8_t capabilities_pointer;
+	uint64_t reserved : 56;
+	uint8_t interrupt_line;
+	uint8_t interrupt_pin;
+	uint8_t min_grant;
+	uint8_t max_latency;
+	uint8_t bus;
+	uint8_t device : 5;
+	uint8_t function : 3;
+	struct PciDevice* next;
+} __attribute__((packed)) PciDevice;
+
+extern PciDevice* pci_devices_list;
+
 void PciInit();
