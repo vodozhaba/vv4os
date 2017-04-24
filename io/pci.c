fc6fd30c99c3bfc5e66b7ee4ff02f80a649847e0
vodozhaba <vodozhaba@vodozhaba.net>
2017-04-24 15:43:18 +0000

    refactoring

--- io/pci.c
+++ io/pci.c
@@ -16,13 +16,6 @@
 #define PCI_CONFIG_ADDRESS_PORT 0xCF8
 #define PCI_CONFIG_DATA_PORT 0xCFC
 
-#define PCI_CONFIG_OFFSET_VENDOR_ID 0x00
-#define PCI_CONFIG_OFFSET_SUBCLASS 0x0A
-#define PCI_CONFIG_OFFSET_CLASS_CODE 0x0B
-#define PCI_CONFIG_OFFSET_HEADER_TYPE 0x0E
-#define PCI_CONFIG_OFFSET_SECONDARY_BUS_NUMBER 0x19
-#define PCI_CONFIG_OFFSET_INTERRUPT_LINE 0x3C
-
 #define PCI_CONFIG_CLASS_CODE_BRIDGE 0x06
 
 #define PCI_CONFIG_SUBCLASS_PCI_TO_PCI_BRIDGE 0x04
@@ -41,11 +34,6 @@ typedef union {
 	} __attribute__((packed)) as_struct;
 	uint32_t as_u32;
 } PciConfigAddress;
-
-typedef struct {
-	uint8_t header_type : 7;
-	bool mf : 1;
-} __attribute__((packed)) PciConfigHeaderType;
 
 PciDevice* pci_devices_list = NULL;
 
@@ -90,25 +78,35 @@ static void PciConfigWrite8(uint8_t bus, uint8_t device, uint8_t function, uint8
 	PciConfigWrite32(bus, device, function, offset, reg);
 }
 
-__attribute__((unused)) static void PciConfigWrite16(uint8_t bus, uint8_t device, uint8_t function, uint8_t offset, uint16_t data) {
+static void PciConfigWrite16(uint8_t bus, uint8_t device, uint8_t function, uint8_t offset, uint16_t data) {
 	uint32_t reg = PciConfigRead32(bus, device, function, offset);
 	reg &= ~(0xFFFF << (offset % 4));
 	reg |= data << (offset % 4);
 	PciConfigWrite32(bus, device, function, offset, reg);
 }
 
-static PciConfigHeaderType PciReadHeaderType(uint8_t bus, uint8_t device, uint8_t function) {
+static PciConfigHeaderType PciConfigReadHeaderType(uint8_t bus, uint8_t device, uint8_t function) {
 	PciConfigHeaderType ret;
-	uint8_t byte = PciConfigRead8(bus, device, function, PCI_CONFIG_OFFSET_HEADER_TYPE);
+	uint8_t byte = PciConfigRead8(bus, device, function, PCI_CONFIG_COMMON_OFFSET_HEADER_TYPE);
 	ret.mf = byte >> 7;
 	ret.header_type = byte;
+	return ret;
+}
+
+static PciConfigBistRegister PciConfigReadBistRegister(uint8_t bus, uint8_t device, uint8_t function) {
+	PciConfigBistRegister ret;
+	uint8_t byte = PciConfigRead8(bus, device, function, PCI_CONFIG_COMMON_OFFSET_BIST_REGISTER);
+	ret.completion_code = byte;
+	ret.reserved = 0;
+	ret.start_bist = byte >> 6;
+	ret.bist_capable = byte >> 7;
 	return ret;
 }
 
 static void PciProbeBus(uint8_t bus);
 
 static void PciAddDevice(uint8_t bus, uint8_t device, uint8_t function) {
-	PciConfigWrite8(bus, device, function, PCI_CONFIG_OFFSET_INTERRUPT_LINE, PCI_IRQ);
+	PciConfigWrite8(bus, device, function, PCI_CONFIG_REGULAR_DEVICE_OFFSET_INTERRUPT_LINE, PCI_IRQ);
 	PciDevice* pci_device = malloc(sizeof(*pci_device));
 	uint32_t* as_arr = (uint32_t*) pci_device;
 	for(size_t i = 0; i < 16; i++) {
@@ -122,26 +120,25 @@ static void PciAddDevice(uint8_t bus, uint8_t device, uint8_t function) {
 }
 
 static void PciProbeFunction(uint8_t bus, uint8_t device, uint8_t function) {
-	uint8_t class = PciConfigRead8(bus, device, function, PCI_CONFIG_OFFSET_CLASS_CODE);
-	uint8_t subclass = PciConfigRead8(bus, device, function, PCI_CONFIG_OFFSET_SUBCLASS);
+	uint8_t class = PciConfigRead8(bus, device, function, PCI_CONFIG_COMMON_OFFSET_CLASS_CODE);
+	uint8_t subclass = PciConfigRead8(bus, device, function, PCI_CONFIG_COMMON_OFFSET_SUBCLASS);
 	if(class == PCI_CONFIG_CLASS_CODE_BRIDGE && subclass == PCI_CONFIG_SUBCLASS_PCI_TO_PCI_BRIDGE) {
-		uint8_t secondary_bus = PciConfigRead8(bus, device, function, PCI_CONFIG_OFFSET_SECONDARY_BUS_NUMBER);
+		uint8_t secondary_bus = PciConfigRead8(bus, device, function, PCI_CONFIG_PCI_TO_PCI_BRIDGE_OFFSET_SECONDARY_BUS_NUMBER);
 		PciProbeBus(secondary_bus);
-		return;
 	}
 	PciAddDevice(bus, device, function);
 }
 
 static void PciProbeDevice(uint8_t bus, uint8_t device) {
-	if(PciConfigRead16(bus, device, 0, PCI_CONFIG_OFFSET_VENDOR_ID) == 0xFFFF)
+	if(PciConfigRead16(bus, device, 0, PCI_CONFIG_COMMON_OFFSET_VENDOR_ID) == 0xFFFF)
 		return;
 	PciProbeFunction(bus, device, 0);
-	PciConfigHeaderType ht = PciReadHeaderType(bus, device, 0);
+	PciConfigHeaderType ht = PciConfigReadHeaderType(bus, device, 0);
 	if(!ht.mf)
 		return;
 
 	for(uint8_t function = 1; function < 8; function++) {
-		if(PciConfigRead16(bus, device, function, PCI_CONFIG_OFFSET_VENDOR_ID) != 0xFFFF)
+		if(PciConfigRead16(bus, device, function, PCI_CONFIG_COMMON_OFFSET_VENDOR_ID) != 0xFFFF)
 			PciProbeFunction(bus, device, function);
 	}
 }
@@ -157,11 +154,11 @@ static void PciIrqHandler() {
 }
 
 void PciInit() {
-	PciConfigHeaderType ht = PciReadHeaderType(0, 0, 0);
+	PciConfigHeaderType ht = PciConfigReadHeaderType(0, 0, 0);
 	PciProbeBus(0);
 	if(ht.mf) {
 		for(uint8_t function = 0; function < 8; function++) {
-			uint16_t vendor_id = PciConfigRead16(0, 0, function, PCI_CONFIG_OFFSET_VENDOR_ID);
+			uint16_t vendor_id = PciConfigRead16(0, 0, function, PCI_CONFIG_COMMON_OFFSET_VENDOR_ID);
 			if(vendor_id == 0xFFFF)
 				break;
 			PciProbeBus(function);
@@ -171,3 +168,34 @@ void PciInit() {
 	X86RegisterIrqHandler(IRQ(PCI_IRQ), PciIrqHandler);
 #endif
 }
+
+uint8_t PciConfigReadFromDevice8(PciDevice* device, uint8_t offset) {
+	return PciConfigRead8(device->bus, device->device, device->function, offset);
+}
+uint16_t PciConfigReadFromDevice16(PciDevice* device, uint8_t offset) {
+	return PciConfigRead16(device->bus, device->device, device->function, offset);
+}
+uint32_t PciConfigReadFromDevice32(PciDevice* device, uint8_t offset) {
+	return PciConfigRead32(device->bus, device->device, device->function, offset);
+}
+
+void PciConfigWriteToDevice8(PciDevice* device, uint8_t offset, uint8_t data) {
+	return PciConfigWrite8(device->bus, device->device, device->function, offset, data);
+}
+void PciConfigWriteToDevice16(PciDevice* device, uint8_t offset, uint16_t data) {
+	return PciConfigWrite16(device->bus, device->device, device->function, offset, data);
+}
+void PciConfigWriteToDevice32(PciDevice* device, uint8_t offset, uint32_t data) {
+	return PciConfigWrite32(device->bus, device->device, device->function, offset, data);
+}
+
+PciConfigHeaderType PciConfigReadHeaderTypeFromDevice(PciDevice* device) {
+	return PciConfigReadHeaderType(device->bus, device->device, device->function);
+}
+
+PciConfigBistRegister PciConfigReadBistRegisterFromDevice(PciDevice* device) {
+	return PciConfigReadBistRegister(device->bus, device->device, device->function);
+}
+
+
+
--- io/pci.h
+++ io/pci.h
@@ -7,43 +7,92 @@
 
 #pragma once
 
+#include <stdbool.h>
 #include <stdint.h>
 
 typedef struct PciDevice {
-	uint16_t vendor_id;
-	uint16_t device_id;
-	uint16_t command;
-	uint16_t status;
-	uint8_t revision_id;
-	uint8_t prog_if;
-	uint8_t subclass;
-	uint8_t class_code;
-	uint8_t cache_line_size;
-	uint8_t latency_timer;
-	uint8_t header_type;
-	uint8_t bist;
-	uint32_t bar0;
-	uint32_t bar1;
-	uint32_t bar2;
-	uint32_t bar3;
-	uint32_t bar4;
-	uint32_t bar5;
-	uint32_t cardbus_cis_pointer;
-	uint16_t subsystem_vendor_id;
-	uint16_t sybsystem_id;
-	uint32_t expansion_rom_base_address;
-	uint8_t capabilities_pointer;
-	uint64_t reserved : 56;
-	uint8_t interrupt_line;
-	uint8_t interrupt_pin;
-	uint8_t min_grant;
-	uint8_t max_latency;
 	uint8_t bus;
 	uint8_t device : 5;
 	uint8_t function : 3;
 	struct PciDevice* next;
 } __attribute__((packed)) PciDevice;
 
+typedef struct {
+	uint8_t header_type : 7;
+	bool mf : 1;
+} __attribute__((packed)) PciConfigHeaderType;
+
+typedef struct {
+	uint8_t completion_code : 4;
+	uint8_t reserved : 2;
+	bool start_bist : 1;
+	bool bist_capable : 1;
+} __attribute__((packed)) PciConfigBistRegister;
+
+#define PCI_CONFIG_COMMON_OFFSET_VENDOR_ID 0x00
+#define PCI_CONFIG_COMMON_OFFSET_DEVICE_ID 0x02
+#define PCI_CONFIG_COMMON_OFFSET_COMMAND 0x04
+#define PCI_CONFIG_COMMON_OFFSET_STATUS 0x06
+#define PCI_CONFIG_COMMON_OFFSET_REVISION_ID 0x08
+#define PCI_CONFIG_COMMON_OFFSET_PROG_IF 0x09
+#define PCI_CONFIG_COMMON_OFFSET_SUBCLASS 0x0A
+#define PCI_CONFIG_COMMON_OFFSET_CLASS_CODE 0x0B
+#define PCI_CONFIG_COMMON_OFFSET_CACHE_LINE_SIZE 0x0C
+#define PCI_CONFIG_COMMON_OFFSET_LATENCY_TIMER 0x0D
+#define PCI_CONFIG_COMMON_OFFSET_HEADER_TYPE 0x0E
+#define PCI_CONFIG_COMMON_OFFSET_BIST_REGISTER 0x0F
+
+#define PCI_CONFIG_REGULAR_DEVICE_OFFSET_BAR0 0x10
+#define PCI_CONFIG_REGULAR_DEVICE_OFFSET_BAR1 0x14
+#define PCI_CONFIG_REGULAR_DEVICE_OFFSET_BAR2 0x18
+#define PCI_CONFIG_REGULAR_DEVICE_OFFSET_BAR3 0x1C
+#define PCI_CONFIG_REGULAR_DEVICE_OFFSET_BAR4 0x20
+#define PCI_CONFIG_REGULAR_DEVICE_OFFSET_BAR5 0x24
+#define PCI_CONFIG_REGULAR_DEVICE_OFFSET_CARDBUS_CIS_POINTER 0x28
+#define PCI_CONFIG_REGULAR_DEVICE_OFFSET_SUBSYSTEM_VENDOR_ID 0x2C
+#define PCI_CONFIG_REGULAR_DEVICE_OFFSET_SUBSYSTEM_ID 0x2E
+#define PCI_CONFIG_REGULAR_DEVICE_OFFSET_EXPANSION_ROM_BASE_ADDRESS 0x30
+#define PCI_CONFIG_REGULAR_DEVICE_OFFSET_CAPABILITIES_POINTER 0x34
+#define PCI_CONFIG_REGULAR_DEVICE_OFFSET_INTERRUPT_LINE 0x3C
+#define PCI_CONFIG_REGULAR_DEVICE_OFFSET_INTERRUPT_PIN 0x3D
+#define PCI_CONFIG_REGULAR_DEVICE_OFFSET_MIN_GRANT 0x3E
+#define PCI_CONFIG_REGULAR_DEVICE_OFFSET_MAX_LATENCY 0x3F
+
+#define PCI_CONFIG_PCI_TO_PCI_BRIDGE_OFFSET_BAR0 0x10
+#define PCI_CONFIG_PCI_TO_PCI_BRIDGE_OFFSET_BAR1 0x14
+#define PCI_CONFIG_PCI_TO_PCI_BRIDGE_OFFSET_PRIMARY_BUS_NUMBER 0x18
+#define PCI_CONFIG_PCI_TO_PCI_BRIDGE_OFFSET_SECONDARY_BUS_NUMBER 0x19
+#define PCI_CONFIG_PCI_TO_PCI_BRIDGE_OFFSET_SUBORDINATE_BUS_NUMBER 0x1A
+#define PCI_CONFIG_PCI_TO_PCI_BRIDGE_OFFSET_SECONDARY_LATENCY_TIMER 0x1B
+#define PCI_CONFIG_PCI_TO_PCI_BRIDGE_OFFSET_IO_BASE 0x1C
+#define PCI_CONFIG_PCI_TO_PCI_BRIDGE_OFFSET_IO_LIMIT 0x1D
+#define PCI_CONFIG_PCI_TO_PCI_BRIDGE_OFFSET_SECONDARY_STATUS 0x1E
+#define PCI_CONFIG_PCI_TO_PCI_BRIDGE_OFFSET_MEMORY_BASE 0x20
+#define PCI_CONFIG_PCI_TO_PCI_BRIDGE_OFFSET_MEMORY_LIMIT 0x22
+#define PCI_CONFIG_PCI_TO_PCI_BRIDGE_OFFSET_PREFETCHABLE_MEMORY_BASE 0x24
+#define PCI_CONFIG_PCI_TO_PCI_BRIDGE_OFFSET_PREFETCHABLE_MEMORY_LIMIT 0x26
+#define PCI_CONFIG_PCI_TO_PCI_BRIDGE_OFFSET_PREFETCHABLE_BASE_UPPER_32_BITS 0x28
+#define PCI_CONFIG_PCI_TO_PCI_BRIDGE_OFFSET_PREFETCHABLE_LIMIT_UPEER_32_BITS 0x2C
+#define PCI_CONFIG_PCI_TO_PCI_BRIDGE_OFFSET_IO_BASE_UPPER_16_BITS 0x30
+#define PCI_CONFIG_PCI_TO_PCI_BRIDGE_OFFSET_IO_LIMIT_UPPER_16_BITS 0x32
+#define PCI_CONFIG_PCI_TO_PCI_BRIDGE_OFFSET_CAPABILITIES_POINTER 0x34
+#define PCI_CONFIG_PCI_TO_PCI_BRIDGE_OFFSET_EXPANSION_ROM_BASE_ADDRESS 0x38
+#define PCI_CONFIG_PCI_TO_PCI_BRIDGE_OFFSET_INTERRUPT_LINE 0x3C
+#define PCI_CONFIG_PCI_TO_PCI_BRIDGE_OFFSET_INTERRUPT_PIN 0x3D
+#define PCI_CONFIG_PCI_TO_PCI_BRIDGE_OFFSET_BRIDGE_CONTROL 0x3E
+
 extern PciDevice* pci_devices_list;
 
 void PciInit();
+
+uint8_t PciConfigReadFromDevice8(PciDevice* device, uint8_t offset);
+uint16_t PciConfigReadFromDevice16(PciDevice* device, uint8_t offset);
+uint32_t PciConfigReadFromDevice32(PciDevice* device, uint8_t offset);
+
+void PciConfigWriteToDevice8(PciDevice* device, uint8_t offset, uint8_t data);
+void PciConfigWriteToDevice16(PciDevice* device, uint8_t offset, uint16_t data);
+void PciConfigWriteToDevice32(PciDevice* device, uint8_t offset, uint32_t data);
+
+PciConfigHeaderType PciConfigReadHeaderTypeFromDevice(PciDevice* device);
+PciConfigBistRegister PciConfigReadBistRegisterFromDevice(PciDevice* device);
+
