e6e4c02c78db573bd7d389fe631e7f66b2c4e081
vodozhaba <vodozhaba@vodozhaba.net>
2017-06-19 20:53:21 +0300

    added config

---
+++ core/config.h
@@ -0,0 +1,11 @@
+/******************************************************************************
+ * File:       /vv4os/core/config.h                                           *
+ * Author:     vodozhaba                                                      *
+ * Created on: Jun 19, 2017                                                   *
+ * Purpose:    Allows configuring VV4OS.                                      *
+ ******************************************************************************/
+
+#pragma once
+
+// If defined, starts a GDB stub at the first COM port in the debugging version.
+#define UART_DEBUGGING
