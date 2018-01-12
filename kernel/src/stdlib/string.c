dc3e5b3ded8a209b0a461c5155d839bee79c8822
vodozhaba <vodozhaba@vodozhaba.net>
2018-01-13 00:57:59 +0300

    Add int strcasecmp(const char*, const char*)

--- kernel/src/stdlib/string.c
+++ kernel/src/stdlib/string.c
@@ -7,14 +7,26 @@
 // Purpose:    Provides str* functions.
 
 #include "string.h"
+#include <ctype.h>
 #include <stddef.h>
 
 int strcmp(const char* str1, const char* str2) {
     size_t i;
-    for(i = 0; str1[i] !='\0' && str2[i] != '\0' && str1[i] == str2[i]; i++);
+    for(i = 0; str1[i] != '\0' && str2[i] != '\0' && str1[i] == str2[i]; i++);
     if(str1[i] < str2[i])
 	    return -1;
     else if(str1[i] == str2[i])
+	    return 0;
+    else
+	    return 1;
+}
+
+int strcasecmp(const char* str1, const char* str2) {
+    size_t i;
+    for(i = 0; str1[i] != '\0' && str2[i] != '\0' && tolower(str1[i]) == tolower(str2[i]); i++);
+    if(tolower(str1[i]) < tolower(str2[i]))
+	    return -1;
+    else if(tolower(str1[i]) == tolower(str2[i]))
 	    return 0;
     else
 	    return 1;
--- kernel/src/stdlib/string.h
+++ kernel/src/stdlib/string.h
@@ -8,6 +8,9 @@
 #include <stddef.h>
 
 int strcmp(const char* str1, const char* str2);
+// Yes, I put a <strings.h> function into <string.h>.
+// Hate me, hit me, hurt me.
+int strcasecmp(const char* str1, const char* str2);
 size_t strlen(const char* str);
 char* strcpy(char* dest, const char* src);
 const char* strchr(const char* str, int character);
